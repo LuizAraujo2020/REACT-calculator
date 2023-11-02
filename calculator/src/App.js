cca9249bd50f6b1b212ac54da608fd8d5a41cf34
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-11-02 15:19:35 -0300

    Função de Subtração e Próximos Passos

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,69 +1,87 @@
 import Input from './components/Input';
 import Button from './components/Button';
 
-import { Container, Content, Row, Column } from './styles';
+import { Container, Content, Row } from './styles';
 import { useState } from 'react';
 
 const App = () => {
   const [currentNumber, setCurrentNumber] = useState('0');
   const [firstNumber, setFirstNumber] = useState('0');
   const [operation, setOperation] = useState('0');
+  const [result, setResult] = useState('0');
 
   const handleOnClear = () => {
     setCurrentNumber('0');
     setFirstNumber('0');
+    setOperation('');
+    setResult('0');
   }
 
   const handleEquals = () => {
-    let result;
     switch (operation) {
       case '+':
-        result = Number(firstNumber) + Number(currentNumber);
+        setResult(Number(firstNumber) + Number(currentNumber));
         break;
       case '-':
-        result = Number(firstNumber) - Number(currentNumber);
+        setResult(Number(firstNumber) - Number(currentNumber));
         break;
       case '*':
-        result = Number(firstNumber) * Number(currentNumber);
+        setResult(Number(firstNumber) * Number(currentNumber));
         break;
       default:
-        result = Number(firstNumber) / Number(currentNumber);
+        setResult(Number(firstNumber) / Number(currentNumber));
         break;
     }
     
-      setCurrentNumber(String(result));
-      setFirstNumber('0');
+      // setCurrentNumber(String(result));
+      // setFirstNumber('0');
+      // setOperation('');
+      // setResult('0');
   }
 
   const handleAddNumber = (number) => {
-    setCurrentNumber(prev => `${prev == '0' ? '' : prev}${number}`);
+    if(result !== '0') {
+      handleOnClear();
+      setCurrentNumber(`${number}`);
+    } else {
+      setCurrentNumber(prev => `${prev === '0' ? '' : prev}${number}`);
+    }
   }
 
-  const handleSumNumbers = (number) => {
-    if(firstNumber == '0') {
+  const handleOperation = (op) => {
+    setOperation(op);
+
+    if(firstNumber === '0') {
       setFirstNumber(currentNumber);
       setCurrentNumber('0');
-    } else {
-      setOperation('+');
-      handleEquals();
+
+    } else if(result !== '0') {
+      setFirstNumber(result);
+      setCurrentNumber('0');
+      setResult('0');
     }
   }
 
   return (
     <Container>
       <Content>
+        <h2>FIR:{firstNumber}</h2>
+        <h2>OPE:{operation}</h2>
+        <h2>CUR:{currentNumber}</h2>
+        <hr/>
+        <h2>RES:{result}</h2>
         <Input value={currentNumber} />
           <Row>
             <Button label="C" onClick={handleOnClear}/>
-            <Button label="*" onClick={() => setOperation('*')}/>
-            <Button label="/" onClick={() => setOperation('/')}/>
-            <Button label="-" onClick={() => setOperation('-')}/>
+            <Button label="*" onClick={() => handleOperation('*')}/>
+            <Button label="/" onClick={() => handleOperation('/')}/>
+            <Button label="-" onClick={() => handleOperation('-')}/>
           </Row>
           <Row>
             <Button label="7" onClick={() => handleAddNumber('7')}/>
             <Button label="8" onClick={() => handleAddNumber('8')}/>
             <Button label="9" onClick={() => handleAddNumber('9')}/>
-            <Button label="+" onClick={handleSumNumbers}/>
+            <Button label="+" onClick={() => handleOperation('+')}/>
           </Row>
           <Row>
             <Button label="4" onClick={() => handleAddNumber('4')}/>
